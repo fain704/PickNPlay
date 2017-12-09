212a5a9e3c75ff0d5ea0e2b170893eb7c86c55be
fain704 <fain704@gmail.com>
2017-12-09 12:47:15 -0500

    git pull

--- models/user.js
+++ models/user.js
@@ -1,45 +1,44 @@
 var Sequelize = require('sequelize');
 var bcrypt = require('bcrypt');
-
+var Pick = require('./Pick.js');
 // create a sequelize instance with our local postgres database information.
-var sequelize = new Sequelize('mysql://root:root@localhost:8889/project2DB');
+var sequelize = require("./connection.js");
 
 // setup User model and its fields.
-var User = sequelize.define('users', {
-    username: {
-        type: Sequelize.STRING,
-        unique: true,
-        allowNull: false
-    },
-    email: {
-        type: Sequelize.STRING,
-        unique: true,
-        allowNull: false
-    },
-    password: {
-        type: Sequelize.STRING,
-        allowNull: false
-    }
+var User = sequelize.define('User', {
+  username: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false
+  },
+  email: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
 }, {
-    hooks: {
-      beforeCreate: (user) => {
-        const salt = bcrypt.genSaltSync();
-        user.password = bcrypt.hashSync(user.password, salt);
-      }
+  hooks: {
+    beforeCreate: (user) => {
+      const salt = bcrypt.genSaltSync();
+      user.password = bcrypt.hashSync(user.password, salt);
     }
+  }
 });
 
 User.prototype.validPassword = function(password) {
-        var passwordCheck = bcrypt.compareSync(password, this.password);
-        console.log("password Check: ", passwordCheck);
-        return passwordCheck;
-      };
+  var passwordCheck = bcrypt.compareSync(password, this.password);
+  console.log("password Check: ", passwordCheck);
+  return passwordCheck;
+};
 
+User.hasMany(Pick, {
+  as: "Picks"
+});
 
-// create all the defined tables in the specified database.
-sequelize.sync()
-    .then(() => console.log('users table has been successfully created, if one doesn\'t exist'))
-    .catch(error => console.log('This error occured', error));
 
 // export User model for use in other files.
 module.exports = User;
