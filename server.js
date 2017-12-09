4a3522ae277d1a25c556667353988bfbbc0476e4
Travis Horne <travis.horne03@gmail.com>
2017-12-09 12:42:01 -0500

    db collection

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 //dependencies
 var express = require('express');
 var bodyParser = require('body-parser');
-
+var Sequelize = require('sequelize');
+var sequelize = require('./models/connection.js');
+const User = require("./models/User.js");
 //set up express app
 var app = express();
 var PORT = process.env.PORT || 8080;
 
 //requiring models
-var db = require('./models');
 
 //set up express app for data parsing
 app.use(bodyParser.json());
@@ -27,8 +28,19 @@ require('./routes/apiRoutes')(app);
 require('./routes/htmlRoutes')(app);
 
 //sync db
-db.sequelize.sync().then(function() {
-  app.listen(PORT, function() {
-    console.log("App listening on port " + PORT);
-  });
-});
+// db.sequelize.sync().then(function() {
+//
+// });
+
+// create all the defined tables in the specified databae.
+sequelize.sync({
+    force: true
+  })
+  .then(() => {
+    console.log('users table has been successfully created, if one doesn\'t exist')
+
+    app.listen(PORT, function() {
+      console.log("App listening on port " + PORT);
+    });
+  })
+  .catch(error => console.log('This error occured', error));
